127bcbe1e9b2b10d49c39adbdda6bb321e239391
Gwynhegg <45057574+Gwynhegg@users.noreply.github.com>
2021-01-28 17:00:06 +0500

    Added .xml input

--- DataInputForm.xaml.cs
+++ DataInputForm.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Xml;
 using System.Data;
 using System.IO;
 using ExcelDataReader;
@@ -22,7 +23,7 @@ namespace HardAnalyzeSys
         private void btnFileInputClick(object sender, RoutedEventArgs e)
         {
             Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog(); //Открываем диалоговое окно
-            dialog.Filter = "Excel files (*.xls*)|*.xls*|.CSV files (*.csv*)|*.csv|.JSON Files (*.json)|*.json|Other Files|*.*"; //задает формат файлов маской
+            dialog.Filter = "Excel files (*.xls*)|*.xls*|.CSV files (*.csv*)|*.csv|.JSON Files (*.json)|*.json|.XML Files (*.xml)|*.xml|Other Files|*.*"; //задает формат файлов маской
             Nullable<bool> result = dialog.ShowDialog();
             string filename = "";
             if (result == true) filename = dialog.FileName;
@@ -33,10 +34,11 @@ namespace HardAnalyzeSys
                 case ".xls": source_table = excelInput(filename);  break;
                 case ".xlsm": source_table = excelInput(filename); break;
                 case ".xlsx": source_table = excelInput(filename); break;
-                case ".csv": source_table = csvInput(filename); break;
+                case ".csv": source_table = customInput(filename, new char[] {','}); break;
                 case ".json": source_table = jsonInput(filename); break;
+                case ".xml": source_table = xmlInput(filename); break;
                 default: try {
-                        source_table = customInput(filename);
+                        source_table = customInput(filename, new char[] { ',', ';', '\t' });
                     }
                     catch
                     {
@@ -55,11 +57,27 @@ namespace HardAnalyzeSys
             }
         }
 
-        private DataTable customInput(string filename)
+        private DataTable xmlInput(string filename)
+        {
+           XmlTextReader reader = new XmlTextReader(filename);
+            DataSet data_set = new DataSet();
+            data_set.ReadXml(reader);
+            DataTable data_table = data_set.Tables[0];
+            try
+            {
+                return data_table;
+            }
+            catch
+            {
+                MessageBox.Show("Возникла ошибка при считывании данных из .XML. Убедитесь в корректности входных данных");
+                return null;
+            }
+        }
+
+        private DataTable customInput(string filename, char[] separators)
         {
             DataTable data_table = new DataTable();
             StreamReader stream_reader = new StreamReader(filename);
-            char[] separators = new char[] { ',', ';', '\t' };
             string[] headers = stream_reader.ReadLine().Split(separators);
             foreach (string header in headers) data_table.Columns.Add(header);
             while (!stream_reader.EndOfStream)
@@ -77,31 +95,6 @@ namespace HardAnalyzeSys
             catch
             {
                 MessageBox.Show("Возникла ошибка при считывании файла. Убедитесь в корректности входных данных");
-                return null;
-            }
-        }
-
-        private DataTable csvInput(string filename)
-        {
-            DataTable data_table = new DataTable();
-            StreamReader stream_reader = new StreamReader(filename);
-            string[] headers = stream_reader.ReadLine().Split(',');
-            foreach (string header in headers) data_table.Columns.Add(header);
-            while (!stream_reader.EndOfStream)
-            {
-                string[] row = stream_reader.ReadLine().Split(',');
-                DataRow data_row = data_table.NewRow();
-                for (int i = 0; i < headers.Length; i++) data_row[i] = row[i];
-                data_table.Rows.Add(data_row);
-            }
-
-            try
-            {
-                return data_table;
-            }
-            catch
-            {
-                MessageBox.Show("Возникла ошибка при считывании .CSV файла. Убедитесь в корректности входных данных");
                 return null;
             }
         }
